f5ad5022e8446d1a2509b530f7a4713fc2e535c1
HaTan1608 <63892386+HaTan1608@users.noreply.github.com>
2021-10-01 22:27:12 +0700

    pp

--- backend/route/emailMsg/emailMsgRoute.js
+++ backend/route/emailMsg/emailMsgRoute.js
@@ -3,7 +3,7 @@ import {authMiddleware} from "../../middlewares/auth/authMiddleware.js";
 import expressAsyncHandler from "express-async-handler";
 import sgMail from "@sendgrid/mail";
 import Filter from "bad-words";
-import EmailMsg from "../../model/EmailMessaging/EmailMessaging.js";
+import EmailMsg from "../../model/emailMessaging/EmailMessaging.js";
 
 const emailMsgRoute = express.Router();
 
